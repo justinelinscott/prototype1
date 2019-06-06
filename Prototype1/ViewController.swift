bcda2dca40bdf5421e5dcdbb3aa1ef9fb7b812df
Justine Linscott <justinelinscott@unomaha.edu>
2019-06-06 13:32:27 -0500

    Initial Commit

---
+++ Prototype1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototype1
+//
+//  Created by Justine Linscott on 6/6/19.
+//  Copyright © 2019 Justine Linscott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
